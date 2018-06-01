8fe23ea36e4b880ae171e6aa1f457979dd4ef837
dannevanzuijlen <36473871+dannevanzuijlen@users.noreply.github.com>
2018-06-01 16:10:03 +0200

    hihih\

---
+++ Project4/app/src/main/java/com/example/remmy/project4/Voorraad.java
@@ -0,0 +1,17 @@
+package com.example.remmy.project4;
+
+public class Voorraad {
+    public String pdtnaam3;
+    public int hdbtermijn3;
+    public String hvlproducten3;
+    public int Minvoorraad;
+    public int Strtdatum;
+
+    public Voorraad(String pdtnaam3, int hdbtermijn3, String hvlproducten3, int Minvoorraad,int Strtdatum ){
+        this.pdtnaam3 = pdtnaam3;
+        this.hdbtermijn3 = hdbtermijn3;
+        this.hvlproducten3 = hvlproducten3;
+        this.Minvoorraad = Minvoorraad;
+        this.Strtdatum = Strtdatum;
+    }
+}
